1caec05d7b4ade21dae881fddceee468edcc3880
Leobritt <leobritto160@gmail.com>
2024-04-23 19:45:37 -0300

    feat: renomeando o dns server application

--- name_resolution_dns/dns-server-app/src/main/java/br/com/everdev/nameresolutiondnsserver/DNSServerApplication.java
+++ name_resolution_dns/dns-server-app/src/main/java/br/com/everdev/nameresolutiondnsserver/DNSServerApplication.java
@@ -1,4 +1,4 @@
-package br.com.everdev.nameresolution.dnsserver;
+package br.com.everdev.nameresolutiondnsserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- name_resolution_dns/dns-server-app/src/main/java/br/com/everdev/nameresolutiondnsserver/controller/DNSServerHealthCheckController.java
+++ name_resolution_dns/dns-server-app/src/main/java/br/com/everdev/nameresolutiondnsserver/controller/DNSServerHealthCheckController.java
@@ -1,5 +1,4 @@
-package br.com.everdev.nameresolution.dnsserver.controller;
-
+package br.com.everdev.nameresolutiondnsserver.controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
