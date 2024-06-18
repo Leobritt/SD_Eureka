48e9c1362a56c2bb2f62a6375baae694c372fbf5
Kauã Vilas Boas <kauacaldeira@hotmail.com>
2024-06-17 23:36:28 -0300

    fix: eureka client

--- name_resolution_dns/dns-server-app/src/main/java/br/com/everdev/nameresolutiondnsserver/controller/DNSServerHealthCheckController.java
+++ name_resolution_dns/dns-server-app/src/main/java/br/com/everdev/nameresolutiondnsserver/controller/DNSServerHealthCheckController.java
@@ -1,21 +1,22 @@
 package br.com.everdev.nameresolutiondnsserver.controller;
 
 
+import com.netflix.discovery.EurekaClient;
+import com.netflix.discovery.shared.Application;
+import com.netflix.discovery.shared.Applications;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class DNSServerHealthCheckController {
+
+    @Autowired
+    private EurekaClient eurekaClient;
 
     @Value("${spring.service-discovery-app}")
     private String applicationDiscoveryURI;
@@ -28,35 +29,17 @@ public class DNSServerHealthCheckController {
         return "Sou o DNS Server e estou online: " + LocalDateTime.now();
     }
 
-    // Método para obter aplicativos registrados no servidor
     @GetMapping("/getRegisteredApplications")
     public List<String> getRegisteredApplications() {
-        try {
-            // Constrói uma solicitação HTTP GET para a URI do servidor de descoberta
-            HttpRequest request = HttpRequest.newBuilder()
-                    .uri(new URI(applicationDiscoveryURI))
-                    .GET()
-                    .header("Content-Type", "application/json")
-                    .header("Accept", "application/json")
-                    .build();
+        List<String> homePageUrls= new ArrayList<>();
+        Applications app = eurekaClient.getApplications();
+        var applications = app.getRegisteredApplications();
 
-            // Envia a solicitação e aguarda a resposta
-            HttpResponse<String> response = HttpClient.newBuilder().build().send(request, HttpResponse.BodyHandlers.ofString());
-
-            // Lista temporária para armazenar URLs de aplicativos
-            List<String> urls = new ArrayList<>();
-            urls.add("http://192.168.0.11:8181/perfil");
-            urls.add("http://192.168.0.11/validar");
-
-            // Retorna a lista de URLs dos aplicativos registrados
-            return urls;
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        } catch (URISyntaxException e) {
-            throw new RuntimeException(e);
+        for(var application : applications){
+            homePageUrls.add(application.getInstances().get(0).getHomePageUrl());
         }
+
+        return homePageUrls;
     }
 
 }
